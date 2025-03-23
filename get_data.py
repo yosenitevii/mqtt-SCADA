a86bf31536c282e84648695201139626b2d5dfe0
yosenitevii <ulascolak7@gmail.com>
2025-03-23 21:53:19 +0300

    backend service
    http server for using the data for frontend

---
+++ get_data.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify
+import psycopg2
+
+# PI0_IP = "localhost"
+PI0_IP = "192.168.1.136" # Raspberry pi Zero's local ip address
+
+app = Flask(__name__)
+
+# Needed variables for connecting to postgre
+DB_CONFIG = {
+    'dbname'    : 'esp8266',
+    'user'      : 'yose',
+    'password'  : '101325',
+    'host'      : PI0_IP,
+    'port'      : 5432
+}
+
+def get_data():
+    try:
+        conn = psycopg2.connect(**DB_CONFIG)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM dht11")
+        rows = cur.fetchall()
+
+        return rows
+    
+    except Exception as e:
+        print("Veritabanı hatası:", e)
+        return []
+    
+    finally:
+        cur.close()
+        conn.close()
+
+@app.route('/data', methods=['GET'])
+def data():
+    rows = get_data()
+    return jsonify(rows)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
