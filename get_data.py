b822b874c341b7dee0290bbc38103a4156a53a3a
yosenitevii <ulascolak7@gmail.com>
2025-05-26 00:21:47 +0300

    working on a SCADA
    Creating a chart.js based UI for data monitoring

--- get_data.py
+++ get_data.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
 import psycopg2
+from datetime import datetime
 
 # PI0_IP = "localhost"
 PI0_IP = "192.168.1.136" # Raspberry pi Zero's local ip address
@@ -15,36 +16,50 @@ DB_CONFIG = {
     'port'      : 5432
 }
 
-def get_data():
+def get_data_by_date(date_str):
     try:
         conn = psycopg2.connect(**DB_CONFIG)
         cur = conn.cursor()
-        cur.execute("SELECT * FROM dht11")
+        cur.execute("SELECT * FROM dht11 WHERE DATE(created_at) = %s ORDER BY created_at ASC", (date_str,))
         rows = cur.fetchall()
 
         # Get column names
         column_names = [desc[0] for desc in cur.description]
-
-        # Transform the data to dictionary
+        
         result = []
         for row in rows:
             row_dict = dict(zip(column_names, row))
-            result.append(row_dict)
 
+            # formatted timestamp
+            if 'created_at' in row_dict and isinstance(row_dict['created_at'], datetime):
+                row_dict['created_at'] = row_dict['created_at'].isoformat()
+
+            result.append(row_dict)
+        
         return result
-    
+        
     except Exception as e:
-        print("Veritabanı hatası:", e)
+        print("Database error:", e)
         return []
     
     finally:
         cur.close()
         conn.close()
 
-@app.route('/data', methods=['GET'])
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/data')
 def data():
-    rows = get_data()
-    return jsonify(rows)
+    date_str = request.args.get('date')
+
+    if not date_str:
+        # Default for today's date
+        date_str = datetime.now().strftime('%Y-%m-%d')
+
+    data = get_data_by_date(date_str)
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
